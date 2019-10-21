976795a2ab8bab8b103dfa4629f9c738e4eb986e
TechStack <TechStackLP@gmail.com>
2019-10-21 09:18:00 -0400

    Working mod that loads

--- src/main/java/com/projectreddog/tsrts/TSRTS.java
+++ src/main/java/com/projectreddog/tsrts/TSRTS.java
@@ -1,5 +1,42 @@
 package com.projectreddog.tsrts;
 
-public class TSRTS {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("tsrts")
+public class TSRTS {
+	// Directly reference a log4j logger.
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	public TSRTS() {
+		// Register the setup method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {
+		// some preinit code
+		LOGGER.info("HELLO FROM PREINIT");
+		LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+	}
+
+	// You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+	// Event bus for receiving Registry Events)
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents {
+		@SubscribeEvent
+		public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+			// register a new block here
+			LOGGER.info("HELLO from Register Block");
+		}
+	}
 }
