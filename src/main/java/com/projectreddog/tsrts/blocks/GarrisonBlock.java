3d54b9773374ad3edf4dcd938c231db9d26f7e05
TechStack <TechStackLP@gmail.com>
2019-10-22 22:07:36 -0400

    Garrison spanws mobs

--- src/main/java/com/projectreddog/tsrts/blocks/GarrisonBlock.java
+++ src/main/java/com/projectreddog/tsrts/blocks/GarrisonBlock.java
@@ -7,8 +7,13 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 public class GarrisonBlock extends Block {
 
@@ -27,4 +32,22 @@ public class GarrisonBlock extends Block {
 
 		return new GarrisonTileEntity();
 	}
+
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
+		super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+
+		if (placer instanceof PlayerEntity) {
+
+			TileEntity te = worldIn.getTileEntity(pos);
+
+			if (te != null) {
+				if (te instanceof GarrisonTileEntity) {
+					GarrisonTileEntity gte = (GarrisonTileEntity) te;
+					gte.setOwner(((PlayerEntity) placer).getScoreboardName());
+				}
+			}
+		}
+	}
+
 }
--- src/main/java/com/projectreddog/tsrts/tileentity/GarrisonTileEntity.java
+++ src/main/java/com/projectreddog/tsrts/tileentity/GarrisonTileEntity.java
@@ -1,20 +1,59 @@
 package com.projectreddog.tsrts.tileentity;
 
+import com.projectreddog.tsrts.entities.MinionEntity;
 import com.projectreddog.tsrts.init.ModBlocks;
+import com.projectreddog.tsrts.init.ModEntities;
 
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 public class GarrisonTileEntity extends TileEntity implements ITickableTileEntity {
 
+	private String onwersName;
+
 	public GarrisonTileEntity() {
 		super(ModBlocks.GARRISON_TILE_ENTITY_TYPE);
 	}
 
+	// TSRTS.LOGGER.info("Owner is :" + getOwner());
+
+	// TSRTS.LOGGER.info(world.getScoreboard().getPlayersTeam(getOwner()).getName());
 	@Override
 	public void tick() {
 		if (!world.isRemote) {
+			if (getOwner() != null) {
+				ModEntities.MINION.spawn(world, null, null, this.pos, SpawnReason.TRIGGERED, true, true);
+
+				MinionEntity me = new MinionEntity(null, world);
+
+				world.addEntity(me);
+			}
 		}
+	}
+
+	public String getOwner() {
+		return onwersName;
+	}
+
+	public void setOwner(String onwersName) {
+		this.onwersName = onwersName;
+		this.markDirty();
+	}
+
+	@Override
+	public CompoundNBT write(CompoundNBT compound) {
+		CompoundNBT nbt = super.write(compound);
+		nbt.putString("owner", onwersName);
+
+		return nbt;
+	}
+
+	@Override
+	public void read(CompoundNBT compound) {
+		super.read(compound);
+		onwersName = compound.getString("owner");
 	}
 
 }
