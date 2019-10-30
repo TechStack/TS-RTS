096357bbe379c9d84dcdd0866cb61f8df1bfda8f
TechStack <TechStackLP@gmail.com>
2019-10-30 17:34:17 -0400

    move to block at a distance

--- src/main/java/com/projectreddog/tsrts/items/SampleItem.java
+++ src/main/java/com/projectreddog/tsrts/items/SampleItem.java
@@ -1,25 +1,22 @@
 package com.projectreddog.tsrts.items;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.projectreddog.tsrts.TSRTS;
-import com.projectreddog.tsrts.entities.UnitEntity;
 import com.projectreddog.tsrts.init.ModItemGroups;
 import com.projectreddog.tsrts.reference.Reference;
+import com.projectreddog.tsrts.utilities.Utilities;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.RayTraceResult.Type;
 import net.minecraft.world.World;
 
 public class SampleItem extends Item {
@@ -37,172 +34,70 @@ public class SampleItem extends Item {
 		}, 0);
 		ItemStack itemstack = playerIn.getHeldItem(handIn);
 		if (entityraytraceresult != null) {
-			TSRTS.LOGGER.info("FOUND" + entityraytraceresult.getEntity().getName());
+			// found entity so attack it if possible !
+			TSRTS.LOGGER.info("Hit entity:" + entityraytraceresult.getEntity().getName());
 			if (entityraytraceresult.getEntity() instanceof LivingEntity) {
 				LivingEntity le = (LivingEntity) entityraytraceresult.getEntity();
+
 				if (!playerIn.world.isRemote) {
 
 					// TODO need to consider if the player is on the same team as the entity or not !
+					Utilities.SelectedUnitsTargetEntity(worldIn, le, playerIn.getScoreboardName());
+				}
+			}
+		} else {
+			// NO ENTITY FOUND so instead look for a block move
 
-					if (TSRTS.playerSelections.containsKey(playerIn.getScoreboardName())) {
-						// found the player in the hasmap get and loop thru the enitties 1
-						int count = TSRTS.playerSelections.get(playerIn.getScoreboardName()).selectedUnits.size();
+			RayTraceResult rtr = playerIn.func_213324_a(32, 1, false);
+			if (rtr != null) {
+				if (rtr.getType() == Type.BLOCK) {
+					if (rtr instanceof BlockRayTraceResult) {
+						BlockRayTraceResult brtr = (BlockRayTraceResult) rtr;
 
-						for (int i = 0; i < TSRTS.playerSelections.get(playerIn.getScoreboardName()).selectedUnits.size(); i++) {
-							UnitEntity ue = (UnitEntity) playerIn.world.getEntityByID(TSRTS.playerSelections.get(playerIn.getScoreboardName()).selectedUnits.get(i));
+						if (!worldIn.isRemote) {
+							TSRTS.LOGGER.info("Block HIT at:" + brtr.getPos());
 
-							if (ue != null) {
-								// TODO check if target is on "Other" team
-								ue.setAttackTarget(le);
-							}
-
+							Utilities.SelectedUnitsMoveToBlock(worldIn, brtr.getPos(), playerIn.getScoreboardName(), playerIn);
 						}
 					}
-
+				} else {
+					// miss
 				}
 			}
 		}
 		return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
 	}
 
-	@Override
-	public ActionResultType onItemUse(ItemUseContext context) {
-		TSRTS.LOGGER.info("CLICKED of:" + context.getPos().up());
-
-		if (!context.getPlayer().world.isRemote) {
-
-			// TODO need to consider if the player is on the same team as the entity or not !
-
-			if (TSRTS.playerSelections.containsKey(context.getPlayer().getScoreboardName())) {
-				// found the player in the hasmap get and loop thru the enitties 1
-				int count = TSRTS.playerSelections.get(context.getPlayer().getScoreboardName()).selectedUnits.size();
-
-				List<BlockPos> lbp = SetFormationPoint(context.getWorld(), context.getPos().up(), count, Direction.getFacingFromVector(context.getPlayer().getLookVec().getX(), 0, context.getPlayer().getLookVec().getZ()));
-
-				if (lbp != null && lbp.size() > 0) {
-					for (int i = 0; i < TSRTS.playerSelections.get(context.getPlayer().getScoreboardName()).selectedUnits.size(); i++) {
-						UnitEntity ue = (UnitEntity) context.getWorld().getEntityByID(TSRTS.playerSelections.get(context.getPlayer().getScoreboardName()).selectedUnits.get(i));
-
-						if (ue != null) {
-							ue.ownerControlledDestination = lbp.get(i);/// context.getPos();
-							TSRTS.LOGGER.info("Destination set to:" + ue.ownerControlledDestination);
-
-						}
-					}
-				}
-			}
-
-		}
-
-		return super.onItemUse(context);
-	}
-
-	@Override
-	public boolean itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand) {
-
-		if (!playerIn.world.isRemote) {
-
-			// TODO need to consider if the player is on the same team as the entity or not !
-
-			if (TSRTS.playerSelections.containsKey(playerIn.getScoreboardName())) {
-				// found the player in the hasmap get and loop thru the enitties 1
-				int count = TSRTS.playerSelections.get(playerIn.getScoreboardName()).selectedUnits.size();
-
-				for (int i = 0; i < TSRTS.playerSelections.get(playerIn.getScoreboardName()).selectedUnits.size(); i++) {
-					UnitEntity ue = (UnitEntity) playerIn.world.getEntityByID(TSRTS.playerSelections.get(playerIn.getScoreboardName()).selectedUnits.get(i));
-
-					if (ue != null) {
-						// TODO check if target is on "Other" team
-						ue.setAttackTarget(target);
-					}
-
-				}
-			}
-
-		}
-
-		return true;
-	}
-
-	public List<BlockPos> SetFormationPoint(World world, BlockPos bp, int size, Direction direction) {
-		List<BlockPos> lbp = new ArrayList<BlockPos>();
-		TSRTS.LOGGER.info("size = :" + size);
-		if (size == 0) {
-			return null;
-		}
-		boolean isEven = ((size % 2) == 0);
-
-		int row = 0;
-		int col = 0;
-		int width = 0;
-		int depth = 0;
-		if (!isEven) {
-			// ODD
-			if (size <= 9) {
-				width = size;
-			} else {
-				width = 9;
-			}
-
-		} else {
-			// even
-			if (size <= 9) {
-				width = size - 1;
-			} else {
-				width = 9;
-			}
-		}
-
-		if (width >= 9) {
-			int tmpDepth = (size / 9);
-			int tmpMod = (size % 9);
-			if (tmpMod > 0) {
-				depth = tmpDepth + 1;
-			} else {
-				depth = tmpDepth;
-			}
-		} else {
-			depth = 1;
-		}
-
-		int widthOffset = 0;
-
-		widthOffset = (width - 1) / 2;
-		for (int j = 0; j < depth; j++) {
-			for (int i = 0; i < width; i++) {
-
-				if ((j + 1) == depth) {
-					// last row
-					int remain = (size - (width * j));
-
-					widthOffset = (remain - 1) / 2;
-
-					if (i < remain) {
-
-						lbp.add(bp.offset(direction.rotateY(), (-widthOffset + i)).offset(direction.getOpposite(), j));
-						SetFormationPoint(world, bp.offset(direction.rotateY(), (-widthOffset + i)).offset(direction.getOpposite(), j));
-					}
-
-				} else {
-
-					lbp.add(bp.offset(direction.rotateY(), (-widthOffset + i)).offset(direction.getOpposite(), j));
-					SetFormationPoint(world, bp.offset(direction.rotateY(), (-widthOffset + i)).offset(direction.getOpposite(), j));
-				}
-
-			}
-		}
-
-		if (isEven && size <= 9) {
-
-			lbp.add(bp.offset(direction.getOpposite(), (depth - 1) + 1));
-			SetFormationPoint(world, bp.offset(direction.getOpposite(), (depth - 1) + 1));
-		}
-		return lbp;
-	}
-
-	public void SetFormationPoint(World world, BlockPos bp) {
-//		world.setBlockState(bp, Blocks.END_ROD.getDefaultState().with(BlockStateProperties.FACING, Direction.UP), 2);
-		/// world.notifyBlockUpdate(bp, Blocks.END_ROD.getDefaultState().with(BlockStateProperties.FACING, Direction.UP), Blocks.END_ROD.getDefaultState().with(BlockStateProperties.FACING, Direction.UP), 2);
-	}
+//	@Override
+//	public ActionResultType onItemUse(ItemUseContext context) {
+//		TSRTS.LOGGER.info("CLICKED of:" + context.getPos().up());
+//
+//		if (!context.getPlayer().world.isRemote) {
+//
+//			// TODO need to consider if the player is on the same team as the entity or not !
+//
+//			if (TSRTS.playerSelections.containsKey(context.getPlayer().getScoreboardName())) {
+//				// found the player in the hasmap get and loop thru the enitties 1
+//				int count = TSRTS.playerSelections.get(context.getPlayer().getScoreboardName()).selectedUnits.size();
+//
+//				List<BlockPos> lbp = SetFormationPoint(context.getWorld(), context.getPos().up(), count, Direction.getFacingFromVector(context.getPlayer().getLookVec().getX(), 0, context.getPlayer().getLookVec().getZ()));
+//
+//				if (lbp != null && lbp.size() > 0) {
+//					for (int i = 0; i < TSRTS.playerSelections.get(context.getPlayer().getScoreboardName()).selectedUnits.size(); i++) {
+//						UnitEntity ue = (UnitEntity) context.getWorld().getEntityByID(TSRTS.playerSelections.get(context.getPlayer().getScoreboardName()).selectedUnits.get(i));
+//
+//						if (ue != null) {
+//							ue.ownerControlledDestination = lbp.get(i);/// context.getPos();
+//							TSRTS.LOGGER.info("Destination set to:" + ue.ownerControlledDestination);
+//
+//						}
+//					}
+//				}
+//			}
+//
+//		}
+//
+//		return super.onItemUse(context);
+//	}
 
 }
--- src/main/java/com/projectreddog/tsrts/utilities/Utilities.java
+++ src/main/java/com/projectreddog/tsrts/utilities/Utilities.java
@@ -1,5 +1,8 @@
 package com.projectreddog.tsrts.utilities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.projectreddog.tsrts.TSRTS;
 import com.projectreddog.tsrts.entities.UnitEntity;
 import com.projectreddog.tsrts.init.ModNetwork;
@@ -7,11 +10,14 @@ import com.projectreddog.tsrts.network.SendTeamInfoPacketToClient;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.scoreboard.ScorePlayerTeam;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -77,6 +83,119 @@ public class Utilities {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public static void SelectedUnitsMoveToBlock(World world, BlockPos target, String ownerName, PlayerEntity player) {
+		if (TSRTS.playerSelections.containsKey(ownerName)) {
+			// found the player in the hasmap get and loop thru the enitties 1
+			int count = TSRTS.playerSelections.get(ownerName).selectedUnits.size();
+
+			List<BlockPos> lbp = SetFormationPoint(world, target.up(), count, Direction.getFacingFromVector(player.getLookVec().getX(), 0, player.getLookVec().getZ()));
+
+			if (lbp != null && lbp.size() > 0) {
+				for (int i = 0; i < TSRTS.playerSelections.get(ownerName).selectedUnits.size(); i++) {
+					UnitEntity ue = (UnitEntity) world.getEntityByID(TSRTS.playerSelections.get(ownerName).selectedUnits.get(i));
+
+					if (ue != null) {
+						ue.ownerControlledDestination = lbp.get(i);/// context.getPos();
+						TSRTS.LOGGER.info("Destination set to:" + ue.ownerControlledDestination);
+
+					}
+				}
+			}
+		}
+	}
+
+	public static void SelectedUnitsTargetEntity(World world, LivingEntity target, String ownerName) {
+
+		if (TSRTS.playerSelections.containsKey(ownerName)) {
+			// found the player in the hasmap get and loop thru the enitties 1
+			int count = TSRTS.playerSelections.get(ownerName).selectedUnits.size();
+
+			for (int i = 0; i < TSRTS.playerSelections.get(ownerName).selectedUnits.size(); i++) {
+				UnitEntity ue = (UnitEntity) world.getEntityByID(TSRTS.playerSelections.get(ownerName).selectedUnits.get(i));
+
+				if (ue != null) {
+					// TODO check if target is on "Other" team
+					ue.setAttackTarget(target);
+				}
+
+			}
+		}
+
+	}
+
+	public static List<BlockPos> SetFormationPoint(World world, BlockPos bp, int size, Direction direction) {
+		List<BlockPos> lbp = new ArrayList<BlockPos>();
+		TSRTS.LOGGER.info("size = :" + size);
+		if (size == 0) {
+			return null;
+		}
+		boolean isEven = ((size % 2) == 0);
+
+		int row = 0;
+		int col = 0;
+		int width = 0;
+		int depth = 0;
+		if (!isEven) {
+			// ODD
+			if (size <= 9) {
+				width = size;
+			} else {
+				width = 9;
+			}
+
+		} else {
+			// even
+			if (size <= 9) {
+				width = size - 1;
+			} else {
+				width = 9;
+			}
+		}
+
+		if (width >= 9) {
+			int tmpDepth = (size / 9);
+			int tmpMod = (size % 9);
+			if (tmpMod > 0) {
+				depth = tmpDepth + 1;
+			} else {
+				depth = tmpDepth;
+			}
+		} else {
+			depth = 1;
+		}
+
+		int widthOffset = 0;
+
+		widthOffset = (width - 1) / 2;
+		for (int j = 0; j < depth; j++) {
+			for (int i = 0; i < width; i++) {
+
+				if ((j + 1) == depth) {
+					// last row
+					int remain = (size - (width * j));
+
+					widthOffset = (remain - 1) / 2;
+
+					if (i < remain) {
+
+						lbp.add(bp.offset(direction.rotateY(), (-widthOffset + i)).offset(direction.getOpposite(), j));
+					}
+
+				} else {
+
+					lbp.add(bp.offset(direction.rotateY(), (-widthOffset + i)).offset(direction.getOpposite(), j));
+				}
+
+			}
+		}
+
+		if (isEven && size <= 9) {
+
+			lbp.add(bp.offset(direction.getOpposite(), (depth - 1) + 1));
+		}
+		return lbp;
 	}
 
 }
