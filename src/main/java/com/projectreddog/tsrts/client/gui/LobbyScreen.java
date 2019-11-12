9f7cd7f1391edda355f2e6ae98d4bfc4bd3fe80f
TechStack <TechStackLP@gmail.com>
2019-11-12 09:56:02 -0500

    minor tweak to lobby gui

--- src/main/java/com/projectreddog/tsrts/client/gui/LobbyScreen.java
+++ src/main/java/com/projectreddog/tsrts/client/gui/LobbyScreen.java
@@ -1,11 +1,14 @@
 package com.projectreddog.tsrts.client.gui;
 
+import java.util.Collection;
+
 import com.projectreddog.tsrts.containers.LobbyContainer;
 import com.projectreddog.tsrts.init.ModNetwork;
 import com.projectreddog.tsrts.network.LobbyGuiButtonClickedPacketToServer;
 import com.projectreddog.tsrts.reference.Reference;
 import com.projectreddog.tsrts.utilities.ClientUtilities;
 import com.projectreddog.tsrts.utilities.Utilities;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.gui.widget.button.Button;
@@ -15,8 +18,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
-
-import java.util.Collection;
 
 public class LobbyScreen extends ContainerScreen<LobbyContainer> {
 
@@ -41,9 +42,9 @@ public class LobbyScreen extends ContainerScreen<LobbyContainer> {
 	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
 		// TODO Auto-generated method stub
 		this.minecraft.getTextureManager().bindTexture(TEXTURE);
-		int x = (this.width - this.xSize) / 2;
+		int x = ((this.width - this.xSize) / 2) - 20;
 
-		int y = (this.height - this.ySize) / 2;
+		int y = ((this.height - this.ySize) / 2) - 20;
 
 		ClientUtilities.renderTexture(x, y, 256, 256);
 		this.blit(x, y, 0, 0, this.xSize, this.ySize);
@@ -58,7 +59,6 @@ public class LobbyScreen extends ContainerScreen<LobbyContainer> {
 		for (NetworkPlayerInfo networkplayerinfo : list) {
 			if (networkplayerinfo != null && networkplayerinfo.getGameProfile() != null) {
 				String playerName = networkplayerinfo.getGameProfile().getName();
-
 
 				Minecraft.getInstance().fontRenderer.drawString(playerName, playerNameColumnPosition, y + 5, 4210752);
 				if (networkplayerinfo.getPlayerTeam() != null) {
@@ -81,19 +81,15 @@ public class LobbyScreen extends ContainerScreen<LobbyContainer> {
 			btnStart.active = false;
 		}
 
-
 	}
-
 
 	@Override
 	protected void init() {
 		super.init();
-		int x = (this.width - this.xSize) / 2;
-		int y = (this.height - this.ySize) / 2;
-
+		int x = ((this.width - this.xSize) / 2) - 20;
+		int y = ((this.height - this.ySize) / 2);
 
 		y = y + 170;
-
 
 		btnReady = addButton(new Button(x + 200, y, 50, 20, "Ready", (button) -> {
 
@@ -139,13 +135,11 @@ public class LobbyScreen extends ContainerScreen<LobbyContainer> {
 
 		}));
 
-
 		btnStart = addButton(new Button(x + 200, y + 20, 50, 20, "Start", (button) -> {
 			ModNetwork.SendToServer(new LobbyGuiButtonClickedPacketToServer(Reference.GUI_BUTTON_LOBBY_START));
 
 		}));
 		btnStart.active = false;
-
 
 //		addButton(new Button(x, y, 50, 10, "Y", (button) -> {
 //			ModNetwork.SendToServer(new TeGuiButtonClickedPacketToServer(pos.getX(), pos.getY(), pos.getZ(), Reference.GUI_BUTTON_DEBUG_TESTERYELLOW));
