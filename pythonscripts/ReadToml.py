00b519e5900b6f2bd786c3461a8af28fbb1c4a09
TechStack <TechStackLP@gmail.com>
2020-09-28 10:54:39 -0400

    adding links between pages in navigation panel

--- pythonscripts/ReadToml.py
+++ pythonscripts/ReadToml.py
@@ -54,7 +54,22 @@ def getAttack_damage(resourecString):
 def getFolow_range(resourecString):
     return splitEntityAttributes(resourecString)[7]
 
+def createPageHeader(doc):
+    with doc.head:
+        link(rel='stylesheet', href='style.css')
+    with doc:
+        with div(id="Header"):
+            img(src="banner.png")
 
+
+def createPageNavBar(doc, Units):
+    with doc:
+        with div(id="NavBar", cls="column" ):
+            h3("Units:")
+            with ul():
+                for U in Units:
+                    li(a(   Units[U]["Name"], href="Unit_"+Units[U]["Name"]+".html"  ) )
+                        
 
 
 def processUnits(config, Units):
@@ -62,16 +77,30 @@ def processUnits(config, Units):
     for U in Units:
         #Create a page for this unit.
         doc = dominate.document(Units[U]["Name"])
+        createPageHeader(doc)
+        div(id="row", cls="row" )
+        createPageNavBar(doc, Units)
         with doc:
-            h1("Unit: "+ Units[U]["Name"] )
-            img(src="./img/" +Units[U]["Name"]  + ".gif")
-        print (Units[U]["Name"])
-        GetUnitCosts(config ,Units[U]["Name"], doc)
-        GetUnitAttributes (config, Units[U]["AttributeName"],doc)
+            with div(id="Text" , cls="column"):
+                GetUnitText(config, Units[U]["Description"])
+        with doc:
+            with div(id="unitDetail",  cls="column" ):
+                h1("Unit: "+ Units[U]["Name"] )
+                img(src="./img/" +Units[U]["Name"]  + ".gif")
+                print (Units[U]["Name"])
+                GetUnitCosts(config ,Units[U]["Name"], doc)
+                GetUnitAttributes (config, Units[U]["AttributeName"],doc)
 
         with open( 'Unit_'+ Units[U]["Name"] +'.html'  , 'w') as file:
             file.write(doc.render())
         print ("")
+
+
+def GetUnitText(config, Description):
+   
+    with div():
+        p(Description)
+
 
 
 def GetUnitAttributes(config, UnitName,doc):
@@ -81,34 +110,34 @@ def GetUnitAttributes(config, UnitName,doc):
         print ("not valid yet for Trebs")
         return 
 
-    with doc:
-        with div():
-            with table():
-                with thead():
-                    with tr():
-                        th("Unit Attributes")
-                with tbody():
-                    with tr():
-                        td("Max Health:")                                
-                        td(getMax_Health(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
-                    with tr():                        
-                        td("Knock back resistance:")
-                        td(getKnock_Back_Resistance(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
-                    with tr():                        
-                        td("Armor: ")
-                        td(getArmor(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
-                    with tr():                        
-                        td("Armor Toughness: ")
-                        td(getArmor_toughness(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
-                    with tr():                        
-                        td("Attack Knockback: ")
-                        td(getAttack_knockback(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
-                    with tr():                        
-                        td("Attack Damage: ")
-                        td(getAttack_damage(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
-                    with tr():                        
-                        td("Follow Range: ")
-                        td(getFolow_range(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
+
+    with div():
+        with table():
+            with thead():
+                with tr():
+                    th("Unit Attributes")
+            with tbody():
+                with tr():
+                    td("Max Health:")                                
+                    td(getMax_Health(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
+                with tr():                        
+                    td("Knock back resistance:")
+                    td(getKnock_Back_Resistance(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
+                with tr():                        
+                    td("Armor: ")
+                    td(getArmor(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
+                with tr():                        
+                    td("Armor Toughness: ")
+                    td(getArmor_toughness(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
+                with tr():                        
+                    td("Attack Knockback: ")
+                    td(getAttack_knockback(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
+                with tr():                        
+                    td("Attack Damage: ")
+                    td(getAttack_damage(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
+                with tr():                        
+                    td("Follow Range: ")
+                    td(getFolow_range(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
 
     print( "Max Health: " +   getMax_Health(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
     print( "Knock back resistance: " +getKnock_Back_Resistance(config['unit_attributes']["unit_"+UnitName+"_attributes"]))
@@ -121,49 +150,49 @@ def GetUnitAttributes(config, UnitName,doc):
 
 def GetUnitCosts(config ,UnitName,doc):
     headers = ["Food", "Wood", "Stone", "Iron", "Gold", "Diamond", "Emerald" ]
-    with doc:
-        with div():
-            with table():
-                with thead():
-                    with tr():
-                        th("Unit Costs")
-                with tbody():
-                    with tr():
-                        with td():
-                             img(src="./img/food.png")
-                        td("Food:")
-                                
-                        td(getFood(config['unit_cost']["unitCosts"+UnitName] ))
-                    with tr():
-                        with td():
-                            img(src="./img/wood.png")
-                        td("Wood:")
-                        td(getWood(config['unit_cost']["unitCosts"+UnitName] ))
-                    with tr():
-                        with td():
-                            img(src="./img/stone.png")
-                        td("Stone:")
-                        td(getStone(config['unit_cost']["unitCosts"+UnitName] ))
-                    with tr():
-                        with td():
-                            img(src="./img/iron.png")
-                        td("Iron:")
-                        td(getIron(config['unit_cost']["unitCosts"+UnitName] ))
-                    with tr():
-                        with td():
-                            img(src="./img/gold.png")
-                        td("Gold:")
-                        td(getGold(config['unit_cost']["unitCosts"+UnitName] ))
-                    with tr():
-                        with td():
-                            img(src="./img/diamond.png")
-                        td("Diamond:")
-                        td(getDiamond(config['unit_cost']["unitCosts"+UnitName] ))
-                    with tr():
-                        with td():
-                            img(src="./img/emerald.png")
-                        td("Emerald:")
-                        td(getEmerald(config['unit_cost']["unitCosts"+UnitName] ))
+    
+    with div():
+        with table():
+            with thead():
+                with tr():
+                    th("Unit Costs")
+            with tbody():
+                with tr():
+                    with td():
+                            img(src="./img/food.png")
+                    td("Food:")
+                            
+                    td(getFood(config['unit_cost']["unitCosts"+UnitName] ))
+                with tr():
+                    with td():
+                        img(src="./img/wood.png")
+                    td("Wood:")
+                    td(getWood(config['unit_cost']["unitCosts"+UnitName] ))
+                with tr():
+                    with td():
+                        img(src="./img/stone.png")
+                    td("Stone:")
+                    td(getStone(config['unit_cost']["unitCosts"+UnitName] ))
+                with tr():
+                    with td():
+                        img(src="./img/iron.png")
+                    td("Iron:")
+                    td(getIron(config['unit_cost']["unitCosts"+UnitName] ))
+                with tr():
+                    with td():
+                        img(src="./img/gold.png")
+                    td("Gold:")
+                    td(getGold(config['unit_cost']["unitCosts"+UnitName] ))
+                with tr():
+                    with td():
+                        img(src="./img/diamond.png")
+                    td("Diamond:")
+                    td(getDiamond(config['unit_cost']["unitCosts"+UnitName] ))
+                with tr():
+                    with td():
+                        img(src="./img/emerald.png")
+                    td("Emerald:")
+                    td(getEmerald(config['unit_cost']["unitCosts"+UnitName] ))
     # print ( "Food: " +  getFood(config['unit_cost']["unitCosts"+UnitName] ))
     # print ( "wood: " +  getWood(config['unit_cost']["unitCosts"+UnitName] ))
     # print ( "Stone: " +  getStone(config['unit_cost']["unitCosts"+UnitName] ))
@@ -188,43 +217,66 @@ def getUnitDict():
     units={
         "Unit1":{
         "Name":"Minion",
-        "AttributeName":"minion"
+        "AttributeName":"minion",
+        "Description":"A basic unit good in hand to hand combat",
+        "Type":"Meele"
         },
         "Unit2":{
         "Name":"Archer",
-        "AttributeName":"archer"
+        "AttributeName":"archer",
+        "Description":"A ranged unit armed with a bow",
+        "Type":"Ranged"
         },
         "Unit3":{
         "Name":"Crossbowmen",
-        "AttributeName":"crossbowman"
+        "AttributeName":"crossbowman",
+        "Description":"An advanced ranged unit armed with a crossbow and years of experience in light leather armor",
+        "Type":"Ranged"
         },
         "Unit4":{
         "Name":"Sapper",
-        "AttributeName":"sapper"
+        "AttributeName":"sapper",
+        "Description":"A demolision expert"        ,
+        "Type":"Siege"
         },
         "Unit5":{
         "Name":"Lancer",
-        "AttributeName":"mounted"
+        "AttributeName":"mounted",
+        "Description":"A mounted unit that relies on his steed for speed and armed with a lance"        ,
+        "Type":"Mounted"
         },
         "Unit6":{
         "Name":"Trebuchet",
-        "AttributeName":"trebuchet"
+        "AttributeName":"trebuchet",
+        "Description":"A ranged sige weapon"        ,
+        "Type":"Siege"
         },
         "Unit7":{
         "Name":"Longbowmen",
-        "AttributeName":"longbowman"
+        "AttributeName":"longbowman",
+        "Description":"An expereicend archer armed with a longbow for greater range"        ,
+        "Type":"Ranged"
         },
         "Unit8":{
         "Name":"AdvancedKnight",
-        "AttributeName":"advanced_knight"
+        "AttributeName":"advanced_knight",
+        "Description":"A Knight in the best armor emeralds can buy"
+        ,
+        "Type":"Meele"
         },
         "Unit9":{
         "Name":"Knight",
-        "AttributeName":"knight"
+        "AttributeName":"knight",
+        "Description":"A Knight in the iron armor"
+        ,
+        "Type":"Meele"
         },
         "Unit10":{
         "Name":"Pikeman",
-        "AttributeName":"pikeman"
+        "AttributeName":"pikeman",
+        "Description":"A meele unit armed with a pike"
+        ,
+        "Type":"Meele"
         }
         
     }
